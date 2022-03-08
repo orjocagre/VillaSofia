6e2b6003f2fa6df981c0da5ccd3700670482bd7d
orjocagre <orlandocaceres114@gmail.com>
2022-03-08 17:20:38 -0600

    Cambio de la ruta de la base de datos local al hosting

--- Datos/ClsDatosConexion.cs
+++ Datos/ClsDatosConexion.cs
@@ -9,15 +9,15 @@ namespace Datos
 {
     public class ClsDatosConexion
     {
-        static string host = "127.0.0.1";
+        /*static string host = "127.0.0.1";
         static string usuario = "root";
         static string clave = "root";
-        static string BaseDatos = "villasofisii";
+        static string BaseDatos = "villasofisii";*/
 
-        /*static string host = "bxz5oysljxjxpbctsnrf-mysql.services.clever-cloud.com";
+        static string host = "bxz5oysljxjxpbctsnrf-mysql.services.clever-cloud.com";
         static string usuario = "ut6e1oluo4pqdxba";
         static string clave = "7Vhkn5USbmWWqfmijAJO";
-        static string BaseDatos = "bxz5oysljxjxpbctsnrf";*/
+        static string BaseDatos = "bxz5oysljxjxpbctsnrf";
 
         public MySqlConnection conectar = new MySqlConnection("datasource=" + host + ";username=" + usuario + ";password=" + clave + ";database=" + BaseDatos);
 
