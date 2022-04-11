2b13d424d803a46c187fbc9b5fee5c5b37c643b8
orjocagre <orlandocaceres114@gmail.com>
2022-04-11 12:39:22 -0600

    PruebaLoging

--- VillaSofia/FrmLoginUsuario.Designer.cs
+++ VillaSofia/FrmLoginUsuario.Designer.cs
@@ -76,7 +76,7 @@ namespace VillaSofia
             this.button1.Font = new System.Drawing.Font("Tw Cen MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.button1.HoverState.Parent = this.button1;
-            this.button1.Location = new System.Drawing.Point(85, 324);
+            this.button1.Location = new System.Drawing.Point(76, 323);
             this.button1.Name = "button1";
             this.button1.ShadowDecoration.Parent = this.button1;
             this.button1.Size = new System.Drawing.Size(122, 37);
